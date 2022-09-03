e5e4121027045bb368feae0dfd3660c566165774
Tasmim Rahman <tasmimrahman2000@gmail.com>
2022-09-03 21:20:30 +0600

    load data added

--- js/news.js
+++ js/news.js
@@ -1,30 +1,23 @@
-// const newsCategories = async() =>{
-//     const url = `https://openapi.programming-hero.com/api/news/categories`
-//     const res = await fetch(url);
-//     const data = await res.json();
-//     // displayNews(news.news);
-//     displayNews(data.data);
-// }
-// const displayNews = categories =>{
-//     const newsContainer = document.getElementById('news-container');
-//     // categories.forEach(category => {
-//     //     const newsAnchor = document.getElementById('a');
-//     //     newsAnchor.classList.add('nav-link');
-//     //     newsAnchor.innerText = ''
-//     // });
-//     for(const category of categories){
-//     console.log(category);
-//     const newsPara = document.createElement('p');
-//     newsPara.classList.add('nav-link');
-//     newsPara.innerText = `news name`;
-//     newsContainer.appendChild(newsPara);
-//     }
+
+const loadData = () => {
+    const url = 'https://openapi.programming-hero.com/api/news/categories'
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayAllData(data.data.news_category))
+        .catch(error => alert(error))
+}
 
 
-//     // const newsAnchor = document.getElementById('a');
-//     // // newsAnchor.classList.add('nav-link active');
-//     // newsAnchor.innerText = 'a';
-//     // newsContainer.appendChild(newsAnchor);
-   
-// }
-// newsCategories();
+const displayAllData = categories => {
+
+    const navContainer = document.getElementById('nav-container');
+    categories.forEach(category => {
+
+        const div = document.createElement('div')
+        div.classList.add('col')
+        div.innerHTML = `<a onclick="setDataById('${category.category_id}')"> ${category.category_name} </a>`
+        navContainer.appendChild(div)
+    
+    })
+}
+loadData()
